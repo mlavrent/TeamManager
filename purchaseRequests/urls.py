f23ac0eb129824b904b1804e118152c19ca34366
Mark Lavrentyev <lavrema@outlook.com>
2018-05-31 16:26:47 +0300

    Create necessary views for the app

--- purchaseRequests/urls.py
+++ purchaseRequests/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path("", views.index, name="index"),
+    path("", views.index, name="index"),
+    path("list/", views.list, name="list"),
+    path("new/", views.new, name="new"),
+    path("<int:pReq_id>", views.detail, name="detail"),
+    path("<int:pReq_id>/edit", views.edit, name="edit"),
 ]
 
--- purchaseRequests/views.py
+++ purchaseRequests/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
 
-# Create your views here.
+def index(request):
+    return HttpResponse("This is the purchase request homepage")
+
+def list(request):
+    return HttpResponse("This is the list page for all the requests")
+
+def new(request):
+    return HttpResponse("This is the page to create a new purchase request")
+
+def detail(request, pReq_id):
+    return HttpResponse("This is the detail page for request %s" % pReq_id)
+
+def edit(request, pReq_id):
+    return HttpResponse("This is the edit page for %s" % pReq_id)
+
