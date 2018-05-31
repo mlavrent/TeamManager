ea837afdf6e0244331c0701477aab7c693f8196f
Mark Lavrentyev <lavrema@outlook.com>
2018-05-31 15:32:51 +0300

    Add Request model

--- purchaseRequests/models.py
+++ purchaseRequests/models.py
@@ -1,3 +1,12 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Request(models.Model):
+    id = models.PositiveIntegerField()
+    timestamp = models.DateTimeField()
+    item = models.CharField(max_length=75)
+    cost = models.DecimalField(decimal_places=2, validators=[MinValueValidator(0.01)])
+    quantity = models.PositiveSmallIntegerField()
+    link = models.URLField(max_length=200)
+    approved = models.NullBooleanField()
