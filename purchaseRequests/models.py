7a92b515ce812ab862a3d19b9813c5684bddefe2
Mark Lavrentyev <lavrema@outlook.com>
2018-05-31 15:36:55 +0300

    Remove id field since it's automatically included

--- purchaseRequests/models.py
+++ purchaseRequests/models.py
@@ -3,10 +3,9 @@ from django.db import models
 
 
 class Request(models.Model):
-    id = models.PositiveIntegerField()
     timestamp = models.DateTimeField()
     item = models.CharField(max_length=75)
-    cost = models.DecimalField(decimal_places=2, validators=[MinValueValidator(0.01)])
+    cost = models.DecimalField(max_digits=6, decimal_places=2, validators=[MinValueValidator(0.01)])
     quantity = models.PositiveSmallIntegerField()
     link = models.URLField(max_length=200)
     approved = models.NullBooleanField()
