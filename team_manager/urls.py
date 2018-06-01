03bbf811b623a686c3ca60ec34001a99f8d04e1e
Mark Lavrentyev <lavrema@outlook.com>
2018-06-01 16:47:31 +0300

    Modify url to be a bit more intuitive

--- team_manager/urls.py
+++ team_manager/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("purchaserequest/", include("purchaseRequests.urls")),
+    path("purchaserequests/", include("purchaseRequests.urls")),
     path("admin/", admin.site.urls),
 ]
