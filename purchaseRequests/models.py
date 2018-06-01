5c14714e49f68906fe7cbb81fa1876610d47a333
Mark Lavrentyev <lavrema@outlook.com>
2018-06-01 16:08:35 +0300

    Modify model a bit for cost field

--- purchaseRequests/models.py
+++ purchaseRequests/models.py
@@ -5,10 +5,13 @@ from django.db import models
 class Request(models.Model):
     timestamp = models.DateTimeField()
     item = models.CharField(max_length=75)
-    cost = models.DecimalField(max_digits=6, decimal_places=2, validators=[MinValueValidator(0.01)])
+    cost = models.DecimalField(max_digits=6, decimal_places=2)
     quantity = models.PositiveSmallIntegerField()
     link = models.URLField(max_length=200)
     approved = models.NullBooleanField()
 
     def __str__(self):
         return self.item + ", x" + str(self.quantity)
+
+    def line_total(self):
+        return self.quantity * self.cost
