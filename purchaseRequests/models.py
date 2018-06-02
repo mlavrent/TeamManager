180d925bba8e1adfc8f34c5d9bd815b10f160ea2
Mark Lavrentyev <lavrema@outlook.com>
2018-06-02 16:56:00 +0300

    Add request author to the request model

--- purchaseRequests/models.py
+++ purchaseRequests/models.py
@@ -1,9 +1,11 @@
 from django.core.validators import MinValueValidator
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Request(models.Model):
     timestamp = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     item = models.CharField(max_length=75)
     cost = models.DecimalField(max_digits=6, decimal_places=2)
     quantity = models.PositiveSmallIntegerField()
