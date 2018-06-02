502abdc2aa14b6b7d4bcbe7df78471bf8bce6296
Mark Lavrentyev <lavrema@outlook.com>
2018-06-02 23:31:21 +0300

    Color approval bar based on request's approval status

--- purchaseRequests/views.py
+++ purchaseRequests/views.py
@@ -14,7 +14,20 @@ def new(request):
 
 def detail(request, pReq_id):
     pur_req = get_object_or_404(Request, pk=pReq_id)
-    return render(request, "purchaseRequests/detail.html", {'pur_req': pur_req})
+    unappr = ""
+    undec = ""
+    appr = ""
+    if pur_req.approved is True:
+        appr = "selected-appr"
+    elif pur_req.approved is False:
+        unappr = "selected-unappr"
+    else:
+        undec = "selected-undec"
+
+    return render(request, "purchaseRequests/detail.html", {'pur_req': pur_req,
+                                                            'unappr_class': unappr,
+                                                            'undec_class': undec,
+                                                            'appr_class': appr,})
 
 def edit(request, pReq_id):
     pur_req = get_object_or_404(Request, pk=pReq_id)
