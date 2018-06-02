9d5183830c5a9d9544539ae130166be9fd0d493a
Mark Lavrentyev <lavrema@outlook.com>
2018-06-02 10:47:59 +0300

    Add basic user login page

--- team_manager/urls.py
+++ team_manager/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("purchaserequests/", include("purchaseRequests.urls")),
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
