259ff8953b0a54b62ffcbbe279829c2aeeec59f9
Mark Lavrentyev <lavrema@outlook.com>
2018-06-06 19:23:26 +0300

    Remove list path to access purchaserequest home page

--- purchaseRequests/urls.py
+++ purchaseRequests/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.list, name="index"),
-    path("list/", views.list, name="list"),
     path("new/", views.new, name="new"),
     path("<int:pReq_id>", views.detail, name="detail"),
     path("<int:pReq_id>/edit", views.edit, name="edit"),
