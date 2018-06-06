7494c184bbbc8e0ea065ba9c968f8340c5bb6982
Mark Lavrentyev <lavrema@outlook.com>
2018-06-06 20:28:32 +0300

    Remove home page and make list as home page

--- purchaseRequests/urls.py
+++ purchaseRequests/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.list, name="index"),
+    path("", views.list, name="list"),
     path("new/", views.new, name="new"),
     path("<int:pReq_id>", views.detail, name="detail"),
     path("<int:pReq_id>/edit", views.edit, name="edit"),
