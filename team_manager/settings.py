0e38475cf0ea2e8bd69f2230fa5c833c90cad590
Mark Lavrentyev <lavrema@outlook.com>
2018-06-09 19:10:31 +0300

    Change database over to postgres instead of sqlite

--- team_manager/settings.py
+++ team_manager/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'team_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'team-manager',
+        'USER': 'postgres',
+        'PASSWORD': 'Hello123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
