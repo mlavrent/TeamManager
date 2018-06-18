e191cc68112b9165a97f4a2625d6eba7a755ca4a
Mark Lavrentyev <lavrema@outlook.com>
2018-06-18 17:06:05 -0400

    Finalize failed login error message

--- team_manager/views.py
+++ team_manager/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import redirect
 from django.contrib.auth import views
+from django.contrib import messages
 
 def redirect_to_login(request):
     return redirect('login', permanent=True)
 
 def login(request, **kwargs):
-    print(request.user.is_authenticated)
     if request.user.is_authenticated:
         return redirect('/purchase-requests', app_name='purchaseRequests')
     else:
