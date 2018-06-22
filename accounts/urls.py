4322d079a8141ad0dd50c1f2ea73a39cf0c9b3d7
Mark Lavrentyev <lavrema@outlook.com>
2018-06-21 22:08:01 -0400

    Move some files for consistency

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "accounts"
 urlpatterns = [
     path("", views.redirect_to_login),
-    path("login/", views.TMLogin.as_view(), name="login"),
-    path("join/", views.TMRegister.as_view(), name="signup"),
+    path("login/", views.login, name="login"),
+    path("join/", views.signup, name="signup"),
     path("", include("django.contrib.auth.urls")),
 ]
