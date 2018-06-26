58b887e95b95f37827269cff0b19e6697cc38431
Mark Lavrentyev <lavrema@outlook.com>
2018-06-26 15:24:43 -0400

    Customize admin user list view

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+
+UserAdmin.list_display = (
+    'username',
+    'email',
+    'first_name',
+    'last_name',
+    'is_active',
+)
