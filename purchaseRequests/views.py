a94a94064a841de440d81bab99f1597eb457ee39
Mark Lavrentyev <lavrema@outlook.com>
2018-06-28 22:40:39 -0400

    Set up responsive coloring on hovers

--- purchaseRequests/views.py
+++ purchaseRequests/views.py
@@ -17,20 +17,19 @@ def list(request):
 @login_required
 def detail(request, pReq_id):
     pur_req = get_object_or_404(Request, pk=pReq_id)
-    unappr = ""
-    undec = ""
-    appr = ""
-    if pur_req.approved is True:
-        appr = "selected-appr"
-    elif pur_req.approved is False:
-        unappr = "selected-unappr"
-    else:
-        undec = "selected-undec"
+    selectable = "sel" if request.user.groups.filter(name="Approver").exists() or request.user.is_superuser else ""
 
-    return render(request, "purchaseRequests/detail.html", {'pur_req': pur_req,
-                                                            'unappr_class': unappr,
-                                                            'undec_class': undec,
-                                                            'appr_class': appr,})
+    state = ["", "", ""]
+    if pur_req.approved is True:
+        state[2] = "current-state"
+    elif pur_req.approved is False:
+        state[0] = "current-state"
+    else:
+        state[1] = "current-state"
+    print(state, selectable)
+    return render(request, "purchaseRequests/detail.html", {"pur_req": pur_req,
+                                                            "state": state,
+                                                            "selectable": selectable,})
 
 
 @login_required
