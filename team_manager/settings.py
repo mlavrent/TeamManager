0472e2ac04631513080f384bbfe31679867573fe
Mark Lavrentyev <lavrema@outlook.com>
2018-12-31 21:03:37 -0500

    Add userbase timezone setting

--- team_manager/settings.py
+++ team_manager/settings.py
@@ -123,6 +123,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Timezone the team is located in
+USER_TZ = 'US/Eastern'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
