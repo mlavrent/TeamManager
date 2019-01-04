f057c9f282fe591c0d7d803eb7e57e9f908c121b
Mark Lavrentyev <lavrema@outlook.com>
2019-01-04 09:57:49 -0500

    Add approver status to user admin site

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,11 +1,20 @@
+from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
 
-UserAdmin.list_display = (
-    'username',
-    'email',
-    'first_name',
-    'last_name',
-    'is_active',
-)
+class TMUserAdmin(UserAdmin):
+    list_display = [
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_approver',
+    ]
+
+    def is_approver(self, user):
+        return user.groups.filter(name="Approvers").exists()
+
+admin.site.unregister(User)
+admin.site.register(User, TMUserAdmin)
