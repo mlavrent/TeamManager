ae45eeeaa77e1cc4c2f3ff31ebece342f1ec8aeb
Mark Lavrentyev <lavrema@outlook.com>
2019-01-03 22:35:57 -0500

    Move db password over to environment variables

--- team_manager/settings.py
+++ team_manager/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'team-manager',
         'USER': 'postgres',
-        'PASSWORD': 'Hello123',
+        'PASSWORD': os.environ.get("DB_PASS"),
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
