3b751054718a75f45bd03f7eb036a5be38498ec0
Mark Lavrentyev <lavrema@outlook.com>
2019-01-09 13:10:40 -0500

    Use jquery for detail page

--- purchaseRequests/static/purchaseRequests/js/detail.js
+++ purchaseRequests/static/purchaseRequests/js/detail.js
@@ -1,19 +1,19 @@
 
 
-window.onload = function() {
-    document.getElementById("acc-button").onclick = function() {
-        noteSpan = document.getElementById("note-span");
-        arrow = document.getElementById("arrow");
-        if(noteSpan.style.display === "none") {
+$(function() {
+    $("#acc-button").click(function() {
+        noteSpan = $("#note-span");
+        arrow = $("#arrow");
+        if(noteSpan.css("display") === "none") {
             // Show note
-            noteSpan.style.display = "initial";
-            arrow.classList.remove("fa-angle-down");
-            arrow.classList.add("fa-angle-up");
+            noteSpan.css("display", "initial");
+            arrow.removeClass("fa-angle-down");
+            arrow.addClass("fa-angle-up");
         } else {
             // Hide note
-            noteSpan.style.display = "none";
-            arrow.classList.remove("fa-angle-up");
-            arrow.classList.add("fa-angle-down");
+            noteSpan.css("display", "none");
+            arrow.removeClass("fa-angle-up");
+            arrow.addClass("fa-angle-down");
         }
-    };
-}
+    });
+});
