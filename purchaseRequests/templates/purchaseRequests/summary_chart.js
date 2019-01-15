9a2049e1bb3ab30d35f3ad67d81d9dbe67d27d68
Mark Lavrentyev <lavrema@outlook.com>
2019-01-15 17:23:13 -0500

    Change style of chart

--- purchaseRequests/templates/purchaseRequests/summary_chart.js
+++ purchaseRequests/templates/purchaseRequests/summary_chart.js
@@ -9,27 +9,32 @@ var summaryChart = new Chart(chartCanvas, {
                 label: 'New requests',
                 backgroundColor: 'rgba(255, 128, 0, 0.4)',
                 borderColor: 'rgba(255, 128, 0, 1)',
+                fill: 'origin',
+                lineTension: 0
             },
             {
                 data: {{ approved_data|safe }},
                 label: 'Approvals',
                 backgroundColor: 'rgba(0, 128, 0, 0.4)',
                 borderColor: 'rgba(0, 128, 0, 1)',
-                fill: '-1'
+                fill: '-1',
+                lineTension: 0
             },
             {
                 data: {{ order_data|safe }},
                 label: 'Purchases',
                 backgroundColor: 'rgba(255, 128, 128, 0.4)',
                 borderColor: 'rgba(255, 128, 128, 1)',
-                fill: '-1'
+                fill: '-1',
+                lineTension: 0
             },
             {
                 data: {{ delivery_data|safe }},
                 label: 'Deliveries',
                 backgroundColor: 'rgba(162, 59, 114, 0.4)',
                 borderColor: 'rgba(162, 59, 114, 1)',
-                fill: '-1'
+                fill: '-1',
+                lineTension: 0
             }
         ]
     },
